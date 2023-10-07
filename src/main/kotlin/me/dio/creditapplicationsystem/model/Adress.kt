14c8e5b4de491e6e4a34c4777cd0b1aa0af03267
Douglas <enjubarte@gmail.com>
2023-10-07 01:35:45 -0300

    Criando modelos e classes de percitências

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/model/Adress.kt
@@ -0,0 +1,12 @@
+package me.dio.creditapplicationsystem.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Adress(
+    @Column(nullable = false)
+    var zipCode: String = "",
+    @Column(nullable = false)
+    var street: String = ""
+)
---
+++ src/main/kotlin/me/dio/creditapplicationsystem/model/Credit.kt
@@ -0,0 +1,24 @@
+package me.dio.creditapplicationsystem.model
+
+import jakarta.persistence.*
+import me.dio.creditapplicationsystem.enums.Status
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.UUID
+@Entity
+data class Credit(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false, unique = true)
+    val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false)
+    val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val dayFirstInstallment: LocalDate,
+    @Column(nullable = false)
+    val numberOfInstallment: Int = 0,
+    @Enumerated
+    val status: Status = Status.IN_PROGRESS,
+    @ManyToOne
+    val customer: Customer? = null,
+)
