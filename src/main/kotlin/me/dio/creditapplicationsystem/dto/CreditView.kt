05f0c059d276963b34b1ca6982f5ea881be3aa78
Douglas <enjubarte@gmail.com>
2023-10-08 16:02:57 -0300

    Finalizando o projeto.

--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditView.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditView.kt
@@ -1,5 +1,7 @@
 package me.dio.creditapplicationsystem.dto
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import me.dio.creditapplicationsystem.enums.Status
 import java.math.BigDecimal
 import java.util.*
@@ -7,6 +9,8 @@ import java.util.*
 data class CreditView(
     val creditCode: UUID,
     val creditValue: BigDecimal,
+    @field:Min(1)
+    @field:Max(48)
     val numberOfInstallment: Int,
     val status: Status,
     val emailCustomer: String,
